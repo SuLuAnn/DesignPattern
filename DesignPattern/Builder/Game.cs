f285aad4613d91c2fab3fe6d0db84a0d410c2b5c
annie_su <LuAnn>
2024-06-19 21:03:46 +0800

    feat : 完成 Builder Pattern 範例 Code

---
+++ DesignPattern/Builder/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPattern.Builder
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+
+        public GameRole CreatenNewbie()
+        {
+            return GameRole.GetBuilder("A").AddBase("村民", "https://base.img")
+                .CreateRole();
+        }
+
+        public GameRole CreatenMagicSwordsman()
+        {
+            return GameRole.GetBuilder("A").AddBase("魔劍士", "https://magicSwordsman.img")
+                .AddAttackSkill("斬擊")
+                .AddDefenseSkill("閃躲")
+                .AddMagicSkill("火球術")
+                .AddLv(100)
+                .CreateRole();
+        }
+    }
+}
---
+++ DesignPattern/Builder/GameRole.cs
@@ -0,0 +1,83 @@
+﻿namespace DesignPattern.Builder
+{
+    public class GameRole
+    {
+        public string Name { get; private set; }
+        public string PictureUrl { get; private set; }
+        public int Lv { get; private set; }
+        public string AttackSkill { get; private set; }
+        public string DefenseSkill { get; private set; }
+        public string MagicSkill { get; private set; }
+
+        private GameRole()
+        {
+        }
+
+        public static IBuilder GetBuilder(string type)
+        {
+            if (type == "A")
+            {
+                return new Builder();
+            }
+            return new Builder();
+        }
+
+        public interface IBuilder
+        {
+            public IBuilder AddBase(string name, string pictureUrl);
+            public IBuilder AddLv(int lv);
+            public IBuilder AddAttackSkill(string attackSkill);
+            public IBuilder AddDefenseSkill(string defenseSkill);
+            public IBuilder AddMagicSkill(string magicSkill);
+
+            public GameRole CreateRole();
+        }
+
+        public class Builder : IBuilder
+        {
+            private GameRole _role;
+
+            public Builder()
+            {
+                _role = new GameRole();
+            }
+
+            public IBuilder AddBase(string name, string pictureUrl)
+            {
+                _role.Name = name;
+                _role.PictureUrl = pictureUrl;
+                _role.Lv = 1;
+                return this;
+            }
+
+            public IBuilder AddAttackSkill(string attackSkill)
+            {
+                _role.AttackSkill = attackSkill;
+                return this;
+            }
+
+            public IBuilder AddDefenseSkill(string defenseSkill)
+            {
+                _role.DefenseSkill = defenseSkill;
+                return this;
+            }
+
+            public IBuilder AddLv(int lv)
+            {
+                _role.Lv = lv;
+                return this;
+            }
+
+            public IBuilder AddMagicSkill(string magicSkill)
+            {
+                _role.MagicSkill = magicSkill;
+                return this;
+            }
+
+            public GameRole CreateRole()
+            {
+                return _role;
+            }
+        }
+    }
+}
